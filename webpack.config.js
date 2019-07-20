b32e2d4bfe05be5683226a69684f05fc09358d06
Juan Carlos <juanksoliveira@gmail.com>
2019-07-20 01:34:06 -0300

    Adicionado browser-sync--webpack-plugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,19 @@
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 module.exports = {
     entry: __dirname + '/src/js/index.js',
     output: {
         path: __dirname + '/public/',
         filename: 'bundle.js'
     },
+    plugins: [
+        new BrowserSyncPlugin({
+            // browse to http://localhost:3000/ during development,
+            // ./public directory is being served
+            host: 'localhost',
+            port: 3000,
+            server: {
+                baseDir: ['public']
+            }
+        })
+    ]
 }
