69db660570903b1dd3232570108b874b901c39d0
Juan Carlos <juanksoliveira@gmail.com>
2019-08-02 01:48:27 -0300

    Adicionado scss, css, imgs, fonts e html loaders.

--- webpack.config.js
+++ webpack.config.js
@@ -10,10 +10,67 @@ module.exports = {
         path: __dirname + '/public/',
         filename: 'bundle.js'
     },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
+    module: {
+        rules: [{
+                test: /\.(scss)$/i,
+                loader: ['style-loader', 'css-loader', 'sass-loader']
             },
+            {
+                test: /\.(css)$/i,
+                use: [{
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            // only enable hot in development
+                            hmr: process.env.NODE_ENV === 'development',
+                            // if hmr does not work, this is a forceful method.
+                            reloadAll: true,
+                        }
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: true,
+                        }
+                    },
+                ],
+            },
+            // Image Loader
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loader: "file-loader",
+                options: {
+                    name: 'img/[name]-[hash].[ext]',
+                },
+            },
+            // Font Loader
+            {
+                test: /\.(eot|ttf|woff2?)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: 'fonts/',
+                        name: '[name].[ext]'
+                    }
+                }]
+            },
+            // HTML Loader
+            {
+                test: /\.html$/i,
+                use: [{
+                    loader: 'html-loader',
+                    options: {
+                        minimize: true,
+                        removeComments: false,
+                        collapseWhitespace: false,
+                        interpolate: true
+                    }
+                }],
+            },
+        ]
+    },
     plugins: [
         new BrowserSyncPlugin({
             // browse to http://localhost:3000/ during development,
