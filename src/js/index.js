96b80b8cbca4cba5c1e5692f0320740351f29957
Juan Carlos <juanksoliveira@gmail.com>
2019-08-02 01:55:05 -0300

    Adicionado index.js pra landing page.

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import '../css/landing-page.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery'
+import 'popper.js'
+
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
