cc3208d6d4132e18187690a371aa4cbd77490a50
Juan Carlos <juanksoliveira@gmail.com>
2019-08-03 22:51:32 -0300

    Alteração da .min do bootstrap e adição do .js.

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import '../css/landing-page.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap';
 import 'jquery'
 import 'popper.js'
 import 'simple-line-icons/scss/simple-line-icons.scss';
