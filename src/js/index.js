d980fe8f29c7c62de68d8bb2253e27af4957907e
Juan Carlos <juanksoliveira@gmail.com>
2019-08-03 22:52:11 -0300

    Adição dos arquivos scss do fontawesome.

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,12 @@ import 'popper.js'
 import 'simple-line-icons/scss/simple-line-icons.scss';
 import 'typeface-lato'
 
+import fontawesome from "@fortawesome/fontawesome-free/scss/fontawesome.scss";
+import regular from "@fortawesome/fontawesome-free/scss/regular.scss";
+import solid from "@fortawesome/fontawesome-free/scss/solid.scss";
+import brands from "@fortawesome/fontawesome-free/scss/brands.scss";
+fontawesome.library.add(solid, regular, brands) 
+
 import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
