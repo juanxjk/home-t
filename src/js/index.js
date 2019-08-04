eaf4af7fa95159705d458023b3d4cbe688224625
Juan Carlos <juanksoliveira@gmail.com>
2019-08-03 21:30:49 -0300

    Adicionado import de simple-line-icons do NPM.

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import '../css/landing-page.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery'
 import 'popper.js'
+import 'simple-line-icons/scss/simple-line-icons.scss';
 import 'typeface-lato'
 
 import '@fortawesome/fontawesome-free/js/fontawesome'
