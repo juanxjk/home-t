5e75022294a7fee0b7e37b01daf50a47d923f697
Juan Carlos <juanksoliveira@gmail.com>
2019-08-07 14:56:42 -0300

    Alteração de dep. .js e .css  para módulos nodejs.

--- src/pages/dashboard/js/dashboard.js
+++ src/pages/dashboard/js/dashboard.js
@@ -1,3 +1,19 @@
+import '../scss/dashboard.scss'
+import 'jquery/dist/jquery'
+import 'popper.js/dist/popper'
+import 'chart.js/dist/Chart'
+
+import fontawesome from "@fortawesome/fontawesome-free/scss/fontawesome.scss";
+import regular from "@fortawesome/fontawesome-free/scss/regular.scss";
+import solid from "@fortawesome/fontawesome-free/scss/solid.scss";
+import brands from "@fortawesome/fontawesome-free/scss/brands.scss";
+fontawesome.library.add(solid, regular, brands) 
+
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+
 (function($) {
   "use strict"; // Start of use strict
 
