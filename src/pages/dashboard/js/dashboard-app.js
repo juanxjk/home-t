e2cd95902a5a8e9b8078b4f4aa67e5ebb1da2d0e
Juan Carlos <juanksoliveira@gmail.com>
2019-08-10 01:10:02 -0300

    Adicionado React app com "hello world".

---
+++ src/pages/dashboard/js/dashboard-app.js
@@ -0,0 +1,11 @@
+import './dashboard';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<p>Hello React</p>, document.getElementById('root'));
+
+
+const dashBoardPage = () => {
+    return (<div> <p>Hello React</p> </div>);
+}
