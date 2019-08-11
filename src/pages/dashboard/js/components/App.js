9c0c2ae1ba2e2bff6cc5a586d947abfbffe32ac3
Juan Carlos <juanksoliveira@gmail.com>
2019-08-10 21:35:18 -0300

    ScrollTopButton e LogoutModal para Component.

--- src/pages/dashboard/js/components/App.js
+++ src/pages/dashboard/js/components/App.js
@@ -7,11 +7,38 @@ import Content from './Content';
 class App extends Component {
 	render() {
 		return (
-			<div id="wrapper">
-				<Sidebar />
-				<div id="content-wrapper" className="d-flex flex-column">
-					<Content />
-					<Footer />
+			<div>
+				<div id="wrapper">
+					<Sidebar />
+					<div id="content-wrapper" className="d-flex flex-column">
+						<Content />
+						<Footer />
+					</div>
+				</div>
+
+				{/* <!-- Scroll to Top Button--> */}
+				<a className="scroll-to-top rounded" href="#page-top">
+					<i className="fas fa-angle-up"></i>
+				</a>
+
+				{/* <!-- Logout Modal--> */}
+				<div className="modal fade" id="logoutModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+					aria-hidden="true">
+					<div className="modal-dialog" role="document">
+						<div className="modal-content">
+							<div className="modal-header">
+								<h5 className="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+								<button className="close" type="button" data-dismiss="modal" aria-label="Close">
+									<span aria-hidden="true">×</span>
+								</button>
+							</div>
+							<div className="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+							<div className="modal-footer">
+								<button className="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+								<a className="btn btn-primary" href="login.html">Logout</a>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		);
