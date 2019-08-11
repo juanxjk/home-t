8504fe4ade60f8d35c87d5a5f252cd678b52483e
Juan Carlos <juanksoliveira@gmail.com>
2019-08-11 20:11:23 -0300

    Tradução de "Searching for" para pt-br.

--- src/app/dashboard/pages/dashboard/components/Topbar.js
+++ src/app/dashboard/pages/dashboard/components/Topbar.js
@@ -15,7 +15,7 @@ class Topbar extends Component {
 					{/* <!-- Topbar Search --> */}
 					<form className="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
 						<div className="input-group">
-							<input type="text" className="form-control bg-light border-0 small" placeholder="Search for..."
+							<input type="text" className="form-control bg-light border-0 small" placeholder="Procurando por..."
 								aria-label="Search" aria-describedby="basic-addon2" />
 							<div className="input-group-append">
 								<button className="btn btn-primary" type="button">
