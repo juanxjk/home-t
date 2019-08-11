5832c671d217fc36c81a0b225ad318d72671b858
Juan Carlos <juanksoliveira@gmail.com>
2019-08-11 20:05:24 -0300

    Adicionado seção de tarefas.

--- src/app/dashboard/pages/dashboard/components/Content.js
+++ src/app/dashboard/pages/dashboard/components/Content.js
@@ -72,13 +72,16 @@ export default class Content extends Component {
                             </div>
                         </div>
 
-                        {/* <!-- Earnings (Monthly) Card Example --> */}
+
+
+
+                        {/* <!-- Tasks --> */}
                         <div className="col-xl-3 col-md-6 mb-4">
                             <div className="card border-left-info shadow h-100 py-2">
                                 <div className="card-body">
                                     <div className="row no-gutters align-items-center">
                                         <div className="col mr-2">
-                                            <div className="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks</div>
+                                            <div className="text-xs font-weight-bold text-info text-uppercase mb-1">Tarefas</div>
                                             <div className="row no-gutters align-items-center">
                                                 <div className="col-auto">
                                                     <div className="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
