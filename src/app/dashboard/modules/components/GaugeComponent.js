651bb6e63f751ea5ae0bd47dd6f7f772bc4c62ed
Juan Carlos <juanksoliveira@gmail.com>
2019-08-11 15:41:16 -0300

    Adição do GaugeComponent.

---
+++ src/app/dashboard/modules/components/GaugeComponent.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class GaugeComponent extends Component {
+    render() {
+        return (
+            <div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h1 class="text-center">Medidor de Nível</h1>
+                            <svg id="fillgauge" width="100%" height="250"></svg>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
