2418ffa799a5eb8bc84c6b5a8ed2a31b5ff59d09
Juan Carlos <juanksoliveira@gmail.com>
2019-08-11 20:05:44 -0300

    Adicionado seção de pedidos de tarefa.

--- src/app/dashboard/pages/dashboard/components/Content.js
+++ src/app/dashboard/pages/dashboard/components/Content.js
@@ -101,13 +101,13 @@ export default class Content extends Component {
                             </div>
                         </div>
 
-                        {/* <!-- Pending Requests Card Example --> */}
+                        {/* <!-- Pending Requests Card --> */}
                         <div className="col-xl-3 col-md-6 mb-4">
                             <div className="card border-left-warning shadow h-100 py-2">
                                 <div className="card-body">
                                     <div className="row no-gutters align-items-center">
                                         <div className="col mr-2">
-                                            <div className="text-xs font-weight-bold text-warning text-uppercase mb-1">Pending Requests</div>
+                                            <div className="text-xs font-weight-bold text-warning text-uppercase mb-1">Pedidos de tarefa</div>
                                             <div className="h5 mb-0 font-weight-bold text-gray-800">18</div>
                                         </div>
                                         <div className="col-auto">
