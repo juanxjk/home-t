5b3783bfd57de77b7c1f1a6dad177a20dacf5aa9
Juan Carlos <juanksoliveira@gmail.com>
2019-08-11 18:36:47 -0300

    Remoção de div em GaugeComponent.

--- src/app/dashboard/modules/components/GaugeComponent.js
+++ src/app/dashboard/modules/components/GaugeComponent.js
@@ -4,15 +4,8 @@ import React, { Component } from 'react'
 export default class GaugeComponent extends Component {
     render() {
         return (
-            <div>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-sm-12">
-                            <h1 className="text-center">Medidor de Nível</h1>
-                            <svg id="fillgauge" width="100%" height="250"></svg>
-                        </div>
-                    </div>
-                </div>
+            <div className="text-center">
+                <svg id="fillgauge" width="100%" height="250"></svg>
             </div>
         )
     }
