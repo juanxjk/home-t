48536ee58c17dc4a27e4ddeab1d32fd770dbfd86
Juan Carlos <juanksoliveira@gmail.com>
2019-08-11 15:43:03 -0300

    Extraído script do html.

---
+++ src/app/dashboard/modules/js/gauge.js
@@ -0,0 +1,86 @@
+
+var config = liquidFillGaugeDefaultSettings();
+config.circleThickness = 0.1;
+config.textVertPosition = 0.2;
+config.waveAnimateTime = 1000;
+config.textVertPosition = 0.5;
+
+var gauge = loadLiquidFillGauge("fillgauge", "-1", config);
+
+function NewValue() {
+    if (Math.random() > 0.5) {
+        return Math.round(Math.random() * 100);
+    } else {
+        return (Math.random() * 100).toFixed(1);
+    }
+}
+const requestApiData = () => {
+    requestURL =
+        "https://www.random.org/integers/?num=1&min=0&max=100&col=1&base=10&format=plain&rnd=new";
+
+    // Create a request variable and assign a new XMLHttpRequest object to it.
+    var request = new XMLHttpRequest();
+    request.responseType = "json";
+
+    // Open a new connection, using the GET request on the URL endpoint
+    request.open("GET", requestURL, true);
+    console.log(request);
+
+    request.onload = function () {
+        // Begin accessing JSON data here
+        if (request.status >= 200 && request.status < 400) {
+            let data = JSON.parse(this.response);
+            console.log(data);
+            const isLow_level = data < 25;
+            const isHigh_level = data > 75;
+            if (isLow_level) {
+                _changeColor({
+                    liquidColor: "#d64d4d",
+                    strokeColor: "#d64d4d",
+                    textColor: "#d64d4d",
+                    textOverlayColor: "#de7171"
+                });
+            } else if (isHigh_level) {
+                _changeColor({
+                    liquidColor: "#7ed650",
+                    strokeColor: "#7ed650",
+                    textColor: "#7ed650",
+                    textOverlayColor: "#bef6b0"
+                });
+            } else {
+                // Default >=25 and <=75
+                _changeColor({
+                    liquidColor: "#178BCA",
+                    strokeColor: "#178BCA",
+                    textColor: "#178BCA",
+                    textOverlayColor: "#47bBfA"
+                });
+            }
+            gauge.update(data);
+        }
+    };
+    const _changeColor = function ({
+        liquidColor = "#178BCA",
+        strokeColor = "#178BCA",
+        textColor = "#178BCA",
+        textOverlayColor = "#47BBFA"
+    }) {
+        d3.select("#fillgauge > g > g > circle").style("fill", liquidColor);
+        d3.select("#fillgauge > g > path").style("fill", strokeColor);
+        d3.select("#fillgauge > g > text").style("fill", textColor);
+        d3.select("#fillgauge > g > g > text").style(
+            "fill",
+            textOverlayColor
+        );
+    };
+
+    request.onerror = function () {
+        console.log("Erro na requisição!");
+        gauge.update("-1");
+    };
+
+    // Send request
+    request.send();
+};
+
+setInterval(requestApiData, 1000);
