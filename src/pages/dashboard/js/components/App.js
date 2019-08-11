0163abad1460ec64a6a3d1940f69c8374994d4b3
Juan Carlos <juanksoliveira@gmail.com>
2019-08-10 21:13:47 -0300

    Correção de layout da página.

--- src/pages/dashboard/js/components/App.js
+++ src/pages/dashboard/js/components/App.js
@@ -7,7 +7,7 @@ import Content from './Content';
 class App extends Component {
 	render() {
 		return (
-			<div>
+			<div id="wrapper">
 				{/* <!-- Begin of Sidebar --> */}
 				<Sidebar></Sidebar>
 				{/* <!-- End of Sidebar --> */}
