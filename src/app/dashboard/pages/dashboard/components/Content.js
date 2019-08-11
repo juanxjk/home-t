56bd8095127f9649244dfbe40d46ddf386d0e03b
Juan Carlos <juanksoliveira@gmail.com>
2019-08-11 20:08:52 -0300

    Adicionado seção Sobre.

--- src/app/dashboard/pages/dashboard/components/Content.js
+++ src/app/dashboard/pages/dashboard/components/Content.js
@@ -310,14 +310,15 @@ export default class Content extends Component {
                                 </div>
                             </div>
 
-                            {/* <!-- Approach --> */}
+                            {/* <!-- About --> */}
                             <div className="card shadow mb-4">
                                 <div className="card-header py-3">
-                                    <h6 className="m-0 font-weight-bold text-primary">Development Approach</h6>
+                                    <h6 className="m-0 font-weight-bold text-primary">Sobre</h6>
                                 </div>
                                 <div className="card-body">
-                                    <p>SB Admin 2 makes extensive use of Bootstrap 4 utility classNamees in order to reduce CSS bloat and poor page performance. Custom CSS classNamees are used to create custom components and custom utility classNamees.</p>
-                                    <p className="mb-0">Before working with this theme, you should become familiar with the Bootstrap framework, especially the utility classNamees.</p>
+                                    <p>Universidade Federal Rural do Semi-Árido</p>
+                                    <p>Projeto da disciplina Tópicos Especiais em Sistemas Digitais</p>
+                                    <p>Autor: Juan Carlos</p>
                                 </div>
                             </div>
 
