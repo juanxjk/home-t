24e76ab16be6a25ae894483d8070705e6ced3c3b
Juan Carlos <juanksoliveira@gmail.com>
2019-08-11 23:10:26 -0300

    Remoção do nav-item charts da Sidebar.

--- src/app/dashboard/pages/dashboard/components/Sidebar.js
+++ src/app/dashboard/pages/dashboard/components/Sidebar.js
@@ -57,10 +57,6 @@ class Sidebar extends Component {
 					</div>
 				</div>
 				</li>
-				{/* <!-- Nav Item - Charts --> */}
-				<li className="nav-item"> <a className="nav-link" href="charts.html">
-					<i className="fas fa-fw fa-chart-area"></i>
-					<span>Charts</span></a> </li>
 				{/* <!-- Nav Item - Settings --> */}
 				<li className="nav-item"> <a className="nav-link" href="#">
 					<i className="fas fa-fw fa-cog"></i> <span>Configurações</span></a> </li> {/* <!-- Divider --> */} <hr className="sidebar-divider d-none d-md-block" />
