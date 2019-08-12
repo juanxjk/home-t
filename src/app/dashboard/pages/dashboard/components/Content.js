8da5a87e2b5385499450641998965c22f21e17dd
Juan Carlos <juanksoliveira@gmail.com>
2019-08-11 23:03:40 -0300

    Remoção da seção de ilustrações.

--- src/app/dashboard/pages/dashboard/components/Content.js
+++ src/app/dashboard/pages/dashboard/components/Content.js
@@ -245,20 +245,6 @@ export default class Content extends Component {
 
                         <div className="col-lg-6 mb-4">
 
-                            {/* <!-- Illustrations --> */}
-                            <div className="card shadow mb-4">
-                                <div className="card-header py-3">
-                                    <h6 className="m-0 font-weight-bold text-primary">Illustrations</h6>
-                                </div>
-                                <div className="card-body">
-                                    <div className="text-center">
-                                        <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{ width: '25rem' }} src={require('../img/undraw_posting_photo.svg')} alt="" />
-                                    </div>
-                                    <p>Add some quality, svg illustrations to your project courtesy of <a target="_blank" rel="nofollow" href="https://undraw.co/">unDraw</a>, a constantly updated collection of beautiful svg images that you can use completely free and without attribution!</p>
-                                    <a target="_blank" rel="nofollow" href="https://undraw.co/">Browse Illustrations on unDraw &rarr;</a>
-                                </div>
-                            </div>
-
                             {/* <!-- About --> */}
                             <div className="card shadow mb-4">
                                 <div className="card-header py-3">
