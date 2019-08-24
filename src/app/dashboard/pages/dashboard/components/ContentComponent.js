f8cd802026ee4b4ae438e1353f703514c7ebc99a
Juan Carlos <juanksoliveira@gmail.com>
2019-08-23 23:49:34 -0300

    Removido MiniCard Pedidos de Tarefa.

--- src/app/dashboard/pages/dashboard/components/ContentComponent.js
+++ src/app/dashboard/pages/dashboard/components/ContentComponent.js
@@ -62,23 +62,6 @@ export default class Content extends Component {
                             fa_icon_color="text-gray-300"
                             progress_bar="50%"
                         />
-                        
-                        {/* <!-- Pending Requests Card --> */}
-                        <div className="col-xl-3 col-md-6 mb-4">
-                            <div className="card border-left-warning shadow h-100 py-2">
-                                <div className="card-body">
-                                    <div className="row no-gutters align-items-center">
-                                        <div className="col mr-2">
-                                            <div className="text-xs font-weight-bold text-warning text-uppercase mb-1">Pedidos de tarefa</div>
-                                            <div className="h5 mb-0 font-weight-bold text-gray-800">18</div>
-                                        </div>
-                                        <div className="col-auto">
-                                            <i className="fas fa-comments fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
                     </div>
 
                     {/* <!-- Content Row --> */}
